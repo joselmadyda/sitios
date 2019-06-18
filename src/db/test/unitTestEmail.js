a3ff5f30dc6347cf24a8e8280e12a4e69648a14f
Alejandro Samsonowicz <geminisentidos@hotmail.com>
2019-06-18 12:16:05 -0300

    Ajustes finales

--- src/db/test/unitTestEmail.js
+++ src/db/test/unitTestEmail.js
@@ -3,9 +3,10 @@ const testEnviarEmail = require('./testGetEmail')
 const serverUrl = 'http://127.0.0.1'
 const serverPort = '8090'
 const api = 'api'
-const seccion = 'email'
+const seccion = 'sitios'
+const modulo = 'email'
 
-const urlEmail = serverUrl + ":" + serverPort + "/" + api + "/" + seccion
+const urlEmail = serverUrl + ":" + serverPort + "/" + api + "/" + seccion + "/" + modulo
 
 async function main() {
 
